000fcf52897e62736ef4c901b65ff522f46d4bfd
Heloisa-albano <Heloisalopesalbano@gmail.com>
2024-04-02 15:52:56 -0300

    Rename campos marca and cor

--- core/models/marca.py
+++ core/models/marca.py
@@ -1,9 +1,9 @@
 from django.db import models 
 
 class Marca(models.Model):
-    descricao = models.CharField(max_length=50)
+    nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, null=True)
 
 
     def __str__(self):
-        return f"{self.descricao.upper()} ({self.id})"
+        return f"{self.nome.upper()} ({self.id})"
